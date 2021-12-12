18ecd52e3c0eec4a606ab1a0dcc15a1eadf3c77b
Paul <pl.sweeney3@gmail.com>
2021-12-12 21:35:34 +0000

    updated for test hosting

--- application.py
+++ application.py
@@ -6,11 +6,11 @@ app = Flask(__name__, static_url_path='', static_folder='.')
 
 @app.route('/')
 def index():
-	return "Hello"
+	return "Hello Testing"
 
-@app.route('iceData/<int:id>')
-def getID(id):
-	return str(id)
+# @app.route('iceData/<int:id>')
+# def getID(id):
+# 	return str(id)
 
 if __name__ == '__main__':
 	app.run(debug = True)
