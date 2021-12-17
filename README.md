0e9044989968f7e47b50d91d38c28a80c54cc790
Paul <pl.sweeney3@gmail.com>
2021-12-17 11:09:38 +0000

    updates

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Data Representation Project 2021
 
 Install required libraries
 pip install /requirements.txt
+
+Activate virtual environment
+```$ source venv/bin/activate```
--- carbonDao.py
+++ carbonDao.py
@@ -1,14 +1,15 @@
 import mysql.connector
 from mysql.connector import cursor
+import DBconfig as cfg
 
 class CarbonDao:
     db = ""
     def __init__(self):
         self.db = mysql.connector.connect(
-            host = 'localhost',
-            user= 'root',
-            password = 'root',
-            database ='embodiedCarbonDB'
+            host =      cfg.mysql['host'],
+            user =      cfg.mysql['user'],
+            password =  cfg.mysql['password'],
+            database =  cfg.mysql['database']
         )
         #print ("connection made")
 
